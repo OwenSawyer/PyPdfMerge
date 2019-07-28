dd2ebd78923150172d503d286b9a4b92519e9dd3
owensawyer <owensawyer95@gmail.com>
2019-07-27 22:21:44 -0400

    Changed file browser to multi select, added file ordering buttons

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # PyPdfMerge
-pip3 install -r requirements.txt
-python3 qt.py
+
+## Installation
+* pip3 install -r requirements.txt
+* python3 qt.py
+
+## TODO
+* File Table
+    * add files -> add to existing list
+    * cancel / delete files
+* Merging
+    * Page specifics per pdf
+    * Specify output file name / location
+* Other
+    * Build / Package as executable
+    * cross platform testing
--- qt.py
+++ qt.py
@@ -1,69 +1,44 @@
-#!/usr/bin/env python
-
-
-#############################################################################
-##
-## Copyright (C) 2013 Riverbank Computing Limited.
-## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-## All rights reserved.
-##
-## This file is part of the examples of PyQt.
-##
-## $QT_BEGIN_LICENSE:BSD$
-## You may use this file under the terms of the BSD license as follows:
-##
-## "Redistribution and use in source and binary forms, with or without
-## modification, are permitted provided that the following conditions are
-## met:
-##   * Redistributions of source code must retain the above copyright
-##     notice, this list of conditions and the following disclaimer.
-##   * Redistributions in binary form must reproduce the above copyright
-##     notice, this list of conditions and the following disclaimer in
-##     the documentation and/or other materials provided with the
-##     distribution.
-##   * Neither the name of Nokia Corporation and its Subsidiary(-ies) nor
-##     the names of its contributors may be used to endorse or promote
-##     products derived from this software without specific prior written
-##     permission.
-##
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-## $QT_END_LICENSE$
-##
-#############################################################################
-
-
 from PyQt5.QtCore import (QDir, QIODevice, QFile, QFileInfo, Qt, QTextStream,
         QUrl)
-from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtGui import QDesktopServices, QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import (QAbstractItemView, QApplication, QComboBox,
-        QDialog, QFileDialog, QGridLayout, QHBoxLayout, QHeaderView, QLabel,
-        QProgressDialog, QPushButton, QSizePolicy, QTableWidget,
-        QTableWidgetItem)
+                             QDialog, QFileDialog, QGridLayout, QHBoxLayout, QHeaderView, QLabel,
+                             QProgressDialog, QPushButton, QSizePolicy, QTableWidget,
+                             QTableWidgetItem, QTableView, QVBoxLayout)
 
+from PyPDF2 import PdfFileMerger
+
+from functools import partial
+
+class PdfMerge():
+
+    def merger(output_path, input_paths):
+        pdf_merger = PdfFileMerger()
+        file_handles = []
+
+        for path in input_paths:
+            pdf_merger.append(path)
+
+        with open(output_path, 'wb') as fileobj:
+            pdf_merger.write(fileobj)
 
 class Window(QDialog):
+
+    DOWN    = 1
+    UP      = -1
+
+
     def __init__(self, parent=None):
         super(Window, self).__init__(parent)
 
         browseButton = self.createButton("&Browse...", self.browse)
-        findButton = self.createButton("&Find", self.find)
+        findButton = self.createButton("&Generate", self.find)
 
         self.fileComboBox = self.createComboBox("*")
         self.textComboBox = self.createComboBox()
         self.directoryComboBox = self.createComboBox(QDir.currentPath())
 
         fileLabel = QLabel("Named:")
-        textLabel = QLabel("Containing text:")
         directoryLabel = QLabel("In directory:")
         self.filesFoundLabel = QLabel()
 
@@ -76,8 +51,15 @@ class Window(QDialog):
         mainLayout = QGridLayout()
         mainLayout.addWidget(fileLabel, 0, 0)
         mainLayout.addWidget(self.fileComboBox, 0, 1, 1, 2)
-        mainLayout.addWidget(textLabel, 1, 0)
-        mainLayout.addWidget(self.textComboBox, 1, 1, 1, 2)
+
+        self.upBtn = QPushButton('Up', self)
+        self.downBtn = QPushButton('Down', self)
+
+        self.buttonLayout = QHBoxLayout()
+        self.buttonLayout.addWidget(self.upBtn)
+        self.buttonLayout.addWidget(self.downBtn)
+        mainLayout.addLayout(self.buttonLayout, 1, 0)
+
         mainLayout.addWidget(directoryLabel, 2, 0)
         mainLayout.addWidget(self.directoryComboBox, 2, 1)
         mainLayout.addWidget(browseButton, 2, 2)
@@ -90,76 +72,29 @@ class Window(QDialog):
         self.resize(700, 300)
 
     def browse(self):
-        directory = QFileDialog.getExistingDirectory(self, "Find Files",
-                QDir.currentPath())
+        filter = "TXT (*.txt);;PDF (*.pdf)"
+        files = QFileDialog.getOpenFileNames(self, caption="Select Files", directory = QDir.currentPath())[0]
+        print(files) # (['E:/Scan_0011.pdf', 'E:/Scan_0013.pdf', 'E:/Scan_0014.pdf', 'E:/Scan_0016.pdf'], 'All Files (*)')
 
-        if directory:
-            if self.directoryComboBox.findText(directory) == -1:
-                self.directoryComboBox.addItem(directory)
-
-            self.directoryComboBox.setCurrentIndex(self.directoryComboBox.findText(directory))
+        self.find(files)
 
     @staticmethod
     def updateComboBox(comboBox):
         if comboBox.findText(comboBox.currentText()) == -1:
             comboBox.addItem(comboBox.currentText())
 
-    def find(self):
+    def find(self, files):
         self.filesTable.setRowCount(0)
 
-        fileName = self.fileComboBox.currentText()
-        text = self.textComboBox.currentText()
         path = self.directoryComboBox.currentText()
-
-        self.updateComboBox(self.fileComboBox)
-        self.updateComboBox(self.textComboBox)
         self.updateComboBox(self.directoryComboBox)
-
         self.currentDir = QDir(path)
-        if not fileName:
-            fileName = "*"
-        files = self.currentDir.entryList([fileName],
-                QDir.Files | QDir.NoSymLinks)
 
-        if text:
-            files = self.findFiles(files, text)
         self.showFiles(files)
-
-    def findFiles(self, files, text):
-        progressDialog = QProgressDialog(self)
-
-        progressDialog.setCancelButtonText("&Cancel")
-        progressDialog.setRange(0, files.count())
-        progressDialog.setWindowTitle("Find Files")
-
-        foundFiles = []
-
-        for i in range(files.count()):
-            progressDialog.setValue(i)
-            progressDialog.setLabelText("Searching file number %d of %d..." % (i, files.count()))
-            QApplication.processEvents()
-
-            if progressDialog.wasCanceled():
-                break
-
-            inFile = QFile(self.currentDir.absoluteFilePath(files[i]))
-
-            if inFile.open(QIODevice.ReadOnly):
-                stream = QTextStream(inFile)
-                while not stream.atEnd():
-                    if progressDialog.wasCanceled():
-                        break
-                    line = stream.readLine()
-                    if text in line:
-                        foundFiles.append(files[i])
-                        break
-
-        progressDialog.close()
-
-        return foundFiles
 
     def showFiles(self, files):
         for fn in files:
+            print(fn)
             file = QFile(self.currentDir.absoluteFilePath(fn))
             size = QFileInfo(file).size()
 
@@ -199,10 +134,52 @@ class Window(QDialog):
 
         self.filesTable.cellActivated.connect(self.openFileOfItem)
 
+        # self.table = QTableView(self)
+        # self.table.setSelectionBehavior(self.table.SelectRows)
+        #
+        # self.model = QStandardItemModel(20, 6, self)
+        # self.table.setModel(self.model)
+
+
     def openFileOfItem(self, row, column):
         item = self.filesTable.item(row, 0)
 
         QDesktopServices.openUrl(QUrl(self.currentDir.absoluteFilePath(item.text())))
+
+    def moveItem(self, row, direction):
+        currentRow = self.filesTable.listWidget.currentRow()
+        currentItem = self.filesTable.listWidget.takeItem(currentRow)
+        if currentRow == 0 or currentRow == len(self.ventana.listWidget):
+            pass
+        else:
+            self.filesTable.listWidget.insertItem(currentRow + direction, currentItem)
+
+    def moveCurrentRow(self, direction=DOWN):
+        if direction not in (self.DOWN, self.UP):
+            return
+
+        model = self.model
+        selModel = self.table.selectionModel()
+        selected = selModel.selectedRows()
+        if not selected:
+            return
+
+        items = []
+        indexes = sorted(selected, key=lambda x: x.row(), reverse=(direction==self.DOWN))
+
+        for idx in indexes:
+            items.append(model.itemFromIndex(idx))
+            rowNum = idx.row()
+            newRow = rowNum+direction
+            if not (0 <= newRow < model.rowCount()):
+                continue
+
+            rowItems = model.takeRow(rowNum)
+            model.insertRow(newRow, rowItems)
+
+        selModel.clear()
+        for item in items:
+            selModel.select(item.index(), selModel.Select|selModel.Rows)
 
 
 if __name__ == '__main__':
@@ -212,4 +189,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Window()
     window.show()
+
     sys.exit(app.exec_())
